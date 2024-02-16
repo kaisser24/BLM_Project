5a9bdb6333f8b0d6da8e07997b03bd444c6ce693
cdomenico8 <caleb.domenico@western.edu>
2024-02-16 09:35:53 -0700

    Create wireless_com.ino

---
+++ wireless_com/wireless_com.ino
@@ -0,0 +1,10 @@
+//This is the file for wireless communication between the arduino and the web app
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
